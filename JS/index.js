26fa910c621af0167d941f503b81286a175d7765
johnhealycodes <93292715+johnhealycodes@users.noreply.github.com>
2023-01-31 19:40:42 -0600

    .

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
 //Pick random class and race from DnD 5e
 //2 buttons that when pressed randomize your class/race in DnD
-const race = document.querySelector('.raceResult')
-const dndClass = document.querySelector('.classResult')
+// const race = document.querySelector('.raceResult')
+// const dndClass = document.querySelector('.classResult')
 document.querySelector('#submitRace').addEventListener('click', pickARace)
 document.querySelector('#submitClass').addEventListener('click', pickAClass)
 
 function pickARace() {
     // $(document).ready(function() {
     //     var rand = Math.floor(Math.random()*3)
-    //     var visibleDiv = $('.raceResult')[rand]
-    //     $(visibleDiv).show()
+    //     var raceResult = $('.raceResult')[rand]
+    //     $(raceResult).show()
     // })
 }
 
@@ -17,7 +17,9 @@ pickARace()
 
 
 function pickAClass() {
-    console.log(`click`)
+    // console.log(`click`)
 }
+
+pickARace()
 
 pickAClass()
